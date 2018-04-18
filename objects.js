cbaa5aa74147b511b51a59044db04e193b5ebcca
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-18 02:05:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,8 @@ playlist["My Bloody Valentine"] = "Sometimes";
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
+  
+  return playlist;
 }
 
 updatePlaylist(playlist, "Phil Ochs", "Here's to Mississippi");
